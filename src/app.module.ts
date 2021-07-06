7e5ded8af6bfac460fa0995a36823e8afbb2db1e
Jeverson <jeversontp@gmail.com>
2021-07-06 19:25:25 -0300

    chore: adicionando .env para configurções

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [
-		JogadoresModule,
-		MongooseModule.forRoot("mongodb+srv://jeverson:PlCn1dm3a38nLh8k@cluster0.xgiud.mongodb.net/smartranking?retryWrites=true&w=majority", {
+
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		MongooseModule.forRoot(process.env.MONGODB_URL, {
 			useNewUrlParser: true,
 			useCreateIndex: true,
 			useUnifiedTopology: true,
 			useFindAndModify: false
-		})
+		}),
+		JogadoresModule,
 	],
 	controllers: [],
 	providers: [],
