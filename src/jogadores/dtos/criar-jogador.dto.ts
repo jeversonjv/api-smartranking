01bcba6ba99854f24bfa037528d06a840da707da
Jeverson <jeversontp@gmail.com>
2021-07-07 11:54:16 -0300

    feat: adicionando Swagger para documentar os endpoints

--- src/jogadores/dtos/criar-jogador.dto.ts
+++ src/jogadores/dtos/criar-jogador.dto.ts
@@ -1,11 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty } from "class-validator";
 export class CriarJogadorDto {
+	@ApiProperty()
 	@IsNotEmpty()
 	readonly telefoneCelular: string;
 
+	@ApiProperty()
 	@IsEmail()
 	readonly email: string;
 
+	@ApiProperty()
 	@IsNotEmpty()
 	readonly nome: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(8080);
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Smart Ranking')
+        .setDescription('API para controlar o ranking de um clube de tênis')
+        .setVersion('1.0')
+        .addTag('Ranking')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(8080);
 }
 bootstrap();
